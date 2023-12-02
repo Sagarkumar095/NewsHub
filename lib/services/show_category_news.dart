6cd864b496dbce791efc36ce074b2c3a85442302
Sagar Kumar <bashu2820159@gmail.com>
2023-12-03 00:32:09 +0530

    initial commit

---
+++ lib/services/show_category_news.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_hub/models/show_category.dart';
+import 'package:news_hub/models/slider_model.dart';
+
+class ShowCategoryNews {
+  List<ShowCategoryModel> categories = [];
+
+  Future<void> getCategoriesNews(String category) async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=899a83203b204fbeb36e8e223549e0a6";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ShowCategoryModel categoryModel = ShowCategoryModel(
+            title: element["title"],
+            description: element["description"],
+            url: element["url"],
+            urlToImage: element["urlToImage"],
+            content: element["content"],
+            author: element["author"],
+          );
+          categories.add(categoryModel);
+        }
+      });
+    }
+  }
+}
